c1dbb733d8dc5c26f2c19a335b3b930de59c45c5
Ago Ben <jehovabalan@gmail.com>
2025-10-15 20:49:10 -0400

    Dynamic wait for API callss to comeplte and network goes idle

--- playwright.config.js
+++ playwright.config.js
@@ -16,10 +16,10 @@ export default defineConfig({
   testDir: './tests' ,
   /* Run tests in files in parallel */
   timeout: 40 * 1000,   //Default is 30 Secs
-    /* general timout 40 secs */
+    /* general timout 40 secs for whole test case */
   expect: {
-    timeout: 40 * 1000
-    /* Expect timeout is 40 secs */
+    timeout: 5000
+    /* Expect assertion timeout is 5 secs */
   },
 
   //
@@ -36,7 +36,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-    browserName: 'webkit', // can be changed to firefox or webkit 
+    browserName: 'chromium', // can be changed to firefox or webkit 
     headless: false,  // true means no browser UI, false means with browser UI
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
@@ -44,42 +44,42 @@ export default defineConfig({
   },
 
   /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+  // projects: [
+  //   {
+  //     name: 'chromium',
+  //     use: { ...devices['Desktop Chrome'] },
+  //   },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+  //   {
+  //     name: 'firefox',
+  //     use: { ...devices['Desktop Firefox'] },
+  //   },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+  //   {
+  //     name: 'webkit',
+  //     use: { ...devices['Desktop Safari'] },
+  //   },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+  //   /* Test against mobile viewports. */
+  //   // {
+  //   //   name: 'Mobile Chrome',
+  //   //   use: { ...devices['Pixel 5'] },
+  //   // },
+  //   // {
+  //   //   name: 'Mobile Safari',
+  //   //   use: { ...devices['iPhone 12'] },
+  //   // },
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
+  //   /* Test against branded browsers. */
+  //   // {
+  //   //   name: 'Microsoft Edge',
+  //   //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+  //   // },
+  //   // {
+  //   //   name: 'Google Chrome',
+  //   //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+  //   // },
+  // ],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -17,7 +17,7 @@ test('Browser Context', async function({browser}) {
     await page.goto('https://www.google.com/')
 });
 
-test.only('Page context', async function({page}) {
+test('Page context', async function({page}) {
    
     /** we jsust going to create empty browser context and page, this can be done by passing 2nd parameter page to test method
     // assign browser to context variable const or let is data type
@@ -27,4 +27,49 @@ test.only('Page context', async function({page}) {
     ***/
     await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
     await expect(page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy')
+    //css-> preferred, not xpath
+    const username = page.locator('#username');
+    const password = page.locator('#password');
+    const signIn = page.locator('#signInBtn');
+    await page.locator('#username').fill('rahulshettyacademy');
+    await page.locator('#password').fill('learning1');
+    await page.locator('#signInBtn').click();
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+    password.fill('learning');
+    await signIn.click();
+    await expect(page).toHaveURL('https://rahulshettyacademy.com/angularpractice/shop');
+    const cards = page.locator('.card-body');
+    const cardCount = await cards.first().textContent();
+    console.log(cardCount);
+    const cardCount2 = await cards.nth(1).textContent();
+    console.log(cardCount2);
+    //allTextContents() is array of text contents of all the elements
+    //it will not wait until page is laoded explicetly, to be aware of it
+     const All = await cards.allTextContents();
+    console.log(All);
+
+});
+
+test.only('Dynamic Wait for API calsls to compelte network idle', async function({page}) {
+   
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
+    await expect(page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy')
+    //css-> preferred, not xpath
+    const username = page.locator('#username');
+    const password = page.locator('#password');
+    const signIn = page.locator('#signInBtn');
+    await page.locator('#username').fill('rahulshettyacademy');
+    password.fill('learning');
+    await signIn.click();
+    await expect(page).toHaveURL('https://rahulshettyacademy.com/angularpractice/shop');
+    const cards = page.locator('.card-body b');
+
+    //wait until behind the api call complete
+     await page.waitForLoadState('networkidle');
+    //allTextContents() is array of text contents of all the elements
+    //it will not wait until page is laoded explicetly, to be aware of it
+     const All = await cards.allTextContents();
+  
+    console.log(All);
+
 });
