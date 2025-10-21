801b608c9cd65d0ab247ea814cd3c74ab88e1095
Ago Ben <jehovabalan@gmail.com>
2025-10-20 21:09:53 -0400

    handle new Window

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -81,7 +81,7 @@ test('Dynamic Wait for API calsls to compelte network idle', async function({pag
 
 });
 
-test.only('UI controls-drpdwn,radiobtn,chkbx', async function({page}) {
+test('UI controls-drpdwn,radiobtn,chkbx', async function({page}) {
    
     await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
     await expect(page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy')
@@ -108,13 +108,66 @@ test.only('UI controls-drpdwn,radiobtn,chkbx', async function({page}) {
 
     await radioButtons.last().isChecked(); // return true or false
    
-        page.pause();
+    page.pause();
     await page.locator('#terms').check();
-     await expect(page.locator('#terms').isChecked).toBeTruthy();
+    expect(await page.locator('#terms').isChecked()).toBeTruthy();
     await page.locator('#terms').uncheck();
-    await expect(page.locator('#terms').isChecked).toBeFalsy();
+    //Action is performed isnide expext hence await inside
+    expect(await page.locator('#terms').isChecked()).toBeFalsy();
     await signIn.click();
     await expect(page).toHaveURL('https://rahulshettyacademy.com/angularpractice/shop');
 
+});
 
+test('UI controls attribute', async function({page}) {
+   
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
+    await expect(page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy')
+    //css-> preferred, not xpath
+    const blinking = page.locator('.blinkingText');
+    //get attribute value
+    await expect(blinking).toHaveAttribute('href','https://rahulshettyacademy.com/documents-request');
+   
+});
+test.only('New tab handling & textcontent() vs inputValue()', async function({browser}) {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
+    await page.waitForLoadState('networkidle');
+    await expect(page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy')
+    //css-> preferred, not xpath
+    const blinking = page.locator('.blinkingText');
+    //get attribute value
+    await expect(blinking).toHaveAttribute('href','https://rahulshettyacademy.com/documents-request');
+
+    //Its a listener from context to listen for another thing to happen
+    //listen for any new page to open in background
+    // const page2 = context.waitForEvent('page')  // this execution in javasvript retuns Promise pending, rejected, fulfilled
+   
+    //Defining areay with waitforevent and clikcing, so that both can go togather.Here dont use await.
+    //if so each will wait for other to complete, that is not expectatation
+    // expectation is parallel.
+    //Among 2 execution, only waitevent return  promise , asssig that to newpage variable
+
+    const [newpage] = await Promise.all([
+        context.waitForEvent('page'),
+        blinking.click(),
+    ])
+    await newpage.waitForLoadState();
+    console.log(await newpage.title());
+    console.log(  await newpage.url());
+    expect(await newpage.title()).toBe('RS Academy');
+    const email = await newpage.locator('.red').textContent();
+  //  console.log(email);
+    const domain = email.split('@')[1].split(' ')[0];
+   // console.log(domain);
+    const username = page.locator('#username');
+    await username.fill(domain);
+  //  await page.pause();
+    await expect(page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy')
+    // it retuens empty, even thugh we progaramtically entered value
+    // textcontent(), considet what value was when page loads
+    console.log(await username.textContent());
+    console.log("--------------------")
+     console.log(await username.inputValue());
 });
