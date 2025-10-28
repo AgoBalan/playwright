0903047823a5e6655eb18fa05153e96385ecab8b
Ago Ben <jehovabalan@gmail.com>
2025-10-27 21:24:39 -0400

    Image comparison and excel dowload upload test cases

---
+++ tests/ExcelDownloadUpload.spec.js
@@ -0,0 +1,18 @@
+const {test,expect,request} = require('@playwright/test');
+
+
+test.only('excel download and upload', async function({page}) {
+
+   await page.goto('https://rahulshettyacademy.com/upload-download-test/');
+
+    //download
+    const downloadPromise = page.waitForEvent('download');
+    await page.getByRole('button',{name:'Download'}).click(); 
+    //wait for download to complete
+    await downloadPromise;   
+   //upload
+    const filePath = './tests/download.xlsx';
+    //setinputfile will work only if element has type='file'in the html attribute
+    await page.locator('#fileinput').setInputFiles(filePath);
+    await page.waitForTimeout(5000);
+});
