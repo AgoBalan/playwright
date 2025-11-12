6cf1dc41f6add3a3af6eec7d3d1228baf3d2eae8
Ago Ben <jehovabalan@gmail.com>
2025-11-11 21:38:49 -0500

    added hooks AFterStep

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,18 +1,25 @@
 const {PageObjectManager} = require('../../PageObjectManager/PageObjectManager');
 const { chromium } = require('playwright');
-const {Before,After} = require("@cucumber/cucumber");
+const {Before,After,AfterStep} = require("@cucumber/cucumber");
 
 // Synchronous
 Before( async function () {
     this.browser = await chromium.launch({ headless: false });
-    const context = await browser.newContext();
-    const page = await context.newPage();
+    const context = await this.browser.newContext();
+    this.page = await context.newPage();
     //World object to share data across steps, should start with this.
-    this.pageObjectManager = new PageObjectManager(page);
+    this.pageObjectManager = new PageObjectManager(this.page);
 });
 
 After(async function () {
   if (this.browser) {
     await this.browser.close();
   }
+});
+
+AfterStep( async function ({result}) {
+  // This hook will be executed after all steps, and take a screenshot on step failure
+ // if (result.status === Status.FAILED) {
+    await this.page.screenshot({ path: `screenshots/CucumberSshot/screenshot-failed-step.png` });
+  //}
 });
